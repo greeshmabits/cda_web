e067efdfc5d4425916326517b89a8fdde89b643e
greeshmabits <2021CFSE007@wilp.bits-pilani.ac.in>
2022-09-11 17:45:50 +0530

    Loginform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const adminUser = {
+    username : "admin",
+    password: "password123",
+  }
+
+  const [user,setUser] = useState({username:"",});
+  const [error,setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.username == adminUser.username && details.password == adminUser.password){
+      console.log("Logged in successfully");
+      setUser({
+        username : details.username,
+      });
+    }
+    else {
+      console.log("Login credentials are wrong!");
+      setError("Details do not match!");
+    }
+
+  }
+
+  const Logout = () => {
+    setUser({username:"",});
+    console.log("Logout");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {(user.username != "") ? (
+      <div className='welcome'>
+        <h2>Welcome <span>{user.username}</span>,</h2>
+        <button onClick={Logout}>Logout</button>
+        </div>
+    ) : (
+      <LoginForm Login={Login} error={error} />
+    ) }
     </div>
   );
 }
