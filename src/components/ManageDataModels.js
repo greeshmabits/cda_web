5ba8c9762763ddf79f4303f57f3bfdb0e1161ef4
greeshmabits <2021CFSE007@wilp.bits-pilani.ac.in>
2022-09-16 16:10:12 +0530

    AnalyzeHistoryAndManageDataModels

--- src/components/ManageDataModels.js
+++ src/components/ManageDataModels.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import React from 'react';
 import * as ReactBootStrap from "react-bootstrap"
+import Table from 'react-bootstrap/Table';
 
 export const ManageDataModels = () => {
 	const navigate = useNavigate()
@@ -9,10 +10,35 @@ export const ManageDataModels = () => {
 		{name:"PCA-Model-2",type:"PCA"},
 		{name:"TRA-Model-1",type:"TRA"},
 	]
+
+	const renderModel = (models,index) => {
+		return (
+			<tr key={index}>
+			<td>{models.name}</td>
+			<td>{models.type}</td>
+			</tr>
+		)
+	}
 	return (
-	  <>
-		<div>ManageDataModels</div>
-		{/* <button onClick={() => navigate('order-summary')}>Place order</button> */}
-	  </>
+	  <form className='manageDataModel-container'>
+		<h1>ManageDataModels</h1>
+		<ReactBootStrap.Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ModelName</th>
+          <th>Type</th>
+          <th>Select</th>
+        </tr>
+      </thead>
+      <tbody>
+		{models.map(renderModel)}
+      </tbody>
+    </ReactBootStrap.Table>
+	<div className='buttons'>
+		<button name='add'>Add</button>
+		<button name='edit'>Edit</button>
+		<button name='delete'>Delete</button>
+	</div>
+	  </form>
 	)
   }
